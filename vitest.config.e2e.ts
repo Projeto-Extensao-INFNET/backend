0ebecf2bd37539f6531168d40199d8c3443516ff
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-07-28 15:24:47 -0300

    refactor: migrate testing framework from Jest to Vitest

---
+++ vitest.config.e2e.ts
@@ -0,0 +1,38 @@
+import { resolve } from 'node:path';
+import swc from 'unplugin-swc';
+import tsConfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+	test: {
+		include: ['**/*.e2e-spec.ts'],
+		globals: true,
+		root: './',
+		environment: 'node',
+		coverage: {
+			reportsDirectory: './coverage-e2e',
+			provider: 'v8',
+			reporter: ['text', 'html', 'lcov', 'cobertura'],
+			include: ['src/**/*.{ts,js}'],
+			exclude: [
+				'**/types/**',
+				'**/*.d.ts',
+				'**/mocks/**',
+				'**/*.test-utils.{ts}',
+				'src/main.ts',
+				'**/*.module.ts',
+			],
+		},
+	},
+	plugins: [
+		tsConfigPaths(),
+		swc.vite({
+			module: { type: 'es6' },
+		}),
+	],
+	resolve: {
+		alias: {
+			src: resolve(__dirname, './src'),
+		},
+	},
+});
---
+++ vitest.config.ts
@@ -0,0 +1,37 @@
+import { resolve } from 'node:path';
+import swc from 'unplugin-swc';
+import tsConfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+	test: {
+		globals: true,
+		root: './',
+		environment: 'node',
+		coverage: {
+			reportsDirectory: './coverage',
+			reporter: ['text', 'html', 'lcov', 'cobertura'],
+			provider: 'v8',
+			include: ['src/**/*.{ts,js}'],
+			exclude: [
+				'**/types/**',
+				'**/*.d.ts',
+				'**/mocks/**',
+				'**/*.test-utils.{ts}',
+				'src/main.ts',
+				'**/*.module.ts',
+			],
+		},
+	},
+	plugins: [
+		tsConfigPaths(),
+		swc.vite({
+			module: { type: 'es6' },
+		}),
+	],
+	resolve: {
+		alias: {
+			src: resolve(__dirname, './src'),
+		},
+	},
+});
