96a2be4ed0632566f19b0b757a9062f64c4e1f1e
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-08-04 19:24:55 -0300

    feat: add min of 8 characters to password and validate cpf and rg format

--- src/schemas/CreateUserBodySchema.ts
+++ src/schemas/CreateUserBodySchema.ts
@@ -1,13 +1,35 @@
 import z from 'zod';
 
-export const CreateUserBodySchema = z.object({
-	name: z.string().min(1),
-	email: z.email(),
-	password: z.string(),
-	birthDate: z.iso.date(),
-	role: z.enum(['PATIENT', 'PROFESSIONAL', 'ADMIN']),
-	documentType: z.enum(['CPF', 'RG']),
-	document: z.string(),
-});
+export const CreateUserBodySchema = z
+	.object({
+		name: z.string().min(1),
+		email: z.email(),
+		password: z.string().min(8),
+		birthDate: z.iso.date(),
+		role: z.enum(['PATIENT', 'PROFESSIONAL', 'ADMIN']),
+		documentType: z.enum(['CPF', 'RG']),
+		document: z.string(),
+	})
+	.refine(
+		(data) => {
+			if (data.documentType === 'CPF') {
+				return /^\d{3}\.\d{3}\.\d{3}-\d{2}$|^\d{11}$/.test(
+					data.document,
+				);
+			}
+
+			if (data.documentType === 'RG') {
+				return /^\d{2}\.\d{3}\.\d{3}-\d{1}$|^\d{9}$/.test(
+					data.document,
+				);
+			}
+
+			return true;
+		},
+		{
+			message: 'Formato de documento inválido para o tipo selecionado',
+			path: ['document'],
+		},
+	);
 
 export type createUserBodySchema = z.infer<typeof CreateUserBodySchema>;
