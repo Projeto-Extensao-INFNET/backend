33ccbf074e4dfa3de2fc8a682ac8d6bb0db466f7
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-08-05 19:51:06 -0300

    refactor: reorganize module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { envSchema } from './config/env';
-// import { AuthModule } from './modules/auth/auth.module';
-import { PrismaModule } from './modules/prisma/prisma.module';
-import { UserModule } from './modules/user/user.module';
+import { envSchema } from '@/config/env';
+import { AuthModule } from '@/modules/auth/auth.module';
+import { PrismaModule } from '@/modules/prisma/prisma.module';
+import { ProfessionalsModule } from '@/modules/professionals/professionals.module';
+import { SpecialtyModule } from '@/modules/specialty/specialty.module';
+import { TreatmentTypeModule } from '@/modules/treatment-type/treatment-type.module';
+import { UserModule } from '@/modules/user/user.module';
 
 @Module({
 	imports: [
@@ -14,7 +17,12 @@ import { UserModule } from './modules/user/user.module';
 		}),
 		PrismaModule,
 		UserModule,
-		// AuthModule,
+		ProfessionalsModule,
+		SpecialtyModule,
+		TreatmentTypeModule,
+		AuthModule,
 	],
+	controllers: [],
+	providers: [],
 })
 export class AppModule {}
