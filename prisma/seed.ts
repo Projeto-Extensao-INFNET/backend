b7ea7bc395a91028dd2c4e24bac80671165be3b1
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-08-05 19:46:26 -0300

    refactor(seed): update seed script

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,24 +4,34 @@
 // ** CRIAR UM USERs COMO PROFISSIONAL [x]
 // ** CRIAR UM USER COMO ADMIN [x]
 // ** CRIAR ESPECIALIDADES (PSIQUIATRA, PSICOLOGO) [x]
-// ** CRIAR UM PROFISSIONAL COM OS RELACIONAMENTOS CORRETOS []
-// ** CRIAR OS TIPOS DE TRATAMENTO (PSIQUIATRIA INFANTIL, CASAIS ETC) []
+// ** CRIAR UM PROFISSIONAL COM OS RELACIONAMENTOS CORRETOS [x]
+// ** CRIAR OS TIPOS DE TRATAMENTO (PSIQUIATRIA INFANTIL, CASAIS ETC) [x]
 // ?? CRIAR A TABELA DE CONEXÃO ENTRE PACIENTE E PROFISSIONAL ( TALVEZ REMOVA )
 // ** CRIAR TABELA DE AGENDA DO PROFISSIONAL []
 // ** CRIAR TABELA DE AGENDA DO PACIENTE []
 
 import { faker } from '@faker-js/faker/locale/pt_BR';
-// ?? devo criar um arquivo entity para cada entidade do banco de dados para ter menos dependência do Prisma?
-// ?? ou apenas criar interfaces e types?
-import { PrismaClient, type Specialty } from '../generated/prisma';
+import {
+	Gender,
+	PaymentMethod,
+	PrismaClient,
+	ProfessionalDocumentType,
+	TypeOfQuery,
+} from '../generated/prisma';
 import { hashPassword } from '../src/utils';
 
 const prisma = new PrismaClient();
 
 const seed = async () => {
+	await prisma.userAgenda.deleteMany();
+	await prisma.schedule.deleteMany();
+	await prisma.professionalAndPatient.deleteMany();
+	await prisma.professional.deleteMany();
 	await prisma.user.deleteMany();
 	await prisma.specialty.deleteMany();
+	await prisma.typesOfTreatment.deleteMany();
 
+	// Criar usuário admin
 	const userAdmin = await prisma.user.create({
 		data: {
 			name: faker.person.fullName(),
@@ -34,6 +44,24 @@ const seed = async () => {
 		},
 	});
 
+	// Criar especialidades
+	await prisma.specialty.createMany({
+		data: [{ name: 'Psiquiatria' }, { name: 'Psicologia' }],
+	});
+
+	// Criar tipos de tratamento
+	await prisma.typesOfTreatment.createMany({
+		data: [
+			{ name: 'Psiquiatria Infantil' },
+			{ name: 'Terapia de Casais' },
+			{ name: 'Terapia Individual' },
+		],
+	});
+
+	const specialties = await prisma.specialty.findMany();
+	const typesOfTreatment = await prisma.typesOfTreatment.findMany();
+
+	// Criar usuários profissionais com seus registros na tabela professionals
 	for (let i = 0; i < 10; i++) {
 		const userProfessional = await prisma.user.create({
 			data: {
@@ -46,7 +74,40 @@ const seed = async () => {
 				document: faker.helpers.replaceSymbols('###.###.###-##'),
 			},
 		});
+
+		await prisma.professional.create({
+			data: {
+				userId: userProfessional.id,
+				typeOfQuery: faker.helpers.arrayElement([
+					TypeOfQuery.ONLINE_VIDEO_CALL,
+					TypeOfQuery.IN_PERSON,
+				]),
+				price: Number(faker.commerce.price({ min: 30, max: 300 })),
+				paymentMethod: faker.helpers.arrayElement([
+					PaymentMethod.CREDIT_CARD,
+					PaymentMethod.PIX,
+				]),
+				documentType: faker.helpers.arrayElement([
+					ProfessionalDocumentType.CRM,
+					ProfessionalDocumentType.CRP,
+				]),
+				document: faker.helpers.replaceSymbols('####/##'),
+				gender: faker.helpers.arrayElement([
+					Gender.MALE,
+					Gender.FEMALE,
+					Gender.NON_BINARY,
+					Gender.TRANSGENDER,
+				]),
+				avatar: faker.image.avatar(),
+				phone: faker.phone.number(),
+				specialtyId: faker.helpers.arrayElement(specialties).id,
+				typeOfTreatmentId:
+					faker.helpers.arrayElement(typesOfTreatment).id,
+			},
+		});
 	}
+
+	// Criar usuários pacientes
 	for (let i = 0; i < 10; i++) {
 		const userPatient = await prisma.user.create({
 			data: {
@@ -60,15 +121,6 @@ const seed = async () => {
 			},
 		});
 	}
-
-	const specialtyList: Specialty[] = [];
-
-	await prisma.specialty.createMany({
-		data: [{ name: 'Psiquiatria' }, { name: 'Psicologia' }],
-	});
-
-	const specialties = await prisma.specialty.findMany();
-	specialtyList.push(...specialties);
 };
 
 seed()
