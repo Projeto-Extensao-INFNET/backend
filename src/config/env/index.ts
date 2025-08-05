45c7681d9a1bd4a05ab37f1f1ccd1349f139a5d2
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-08-04 23:00:29 -0300

    refactor: remove 'e2e' environment option from NODE_ENV schema

--- src/config/env/index.ts
+++ src/config/env/index.ts
@@ -4,7 +4,7 @@ export const envSchema = z.object({
 	DATABASE_URL: z.url().startsWith('postgresql://'),
 	PORT: z.coerce.number().optional().default(3333),
 	NODE_ENV: z
-		.enum(['development', 'test', 'e2e', 'production'])
+		.enum(['development', 'test', 'production'])
 		.default('development'),
 	CORS_ORIGIN: z.url().startsWith('http://localhost:'),
 });
