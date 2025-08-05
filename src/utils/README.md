d2e303c7f7262521691c52a25d1ac6641ec2ebbd
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-08-04 22:37:18 -0300

    test(utils): add unit tests for utility functions

--- src/utils/README.md
+++
@@ -1,3 +0,0 @@
-# utils
-
-Esta pasta contém funções utilitárias e helpers reutilizáveis em diferentes partes do projeto. O objetivo é evitar duplicação de código e facilitar a manutenção de funcionalidades comuns.
---
+++ src/utils/utils.spec.ts
@@ -0,0 +1,31 @@
+import { comparePassword, generateUUID, hashPassword } from '@/utils';
+
+describe('Utils testing', () => {
+	it('should hash password correctly', async () => {
+		const password = '12345678';
+
+		const hashedPassword = await hashPassword(password);
+
+		const isPasswordHashed = await comparePassword(
+			password,
+			hashedPassword,
+		);
+
+		const isPasswordWrong = await comparePassword(
+			hashedPassword,
+			'87654321',
+		);
+
+		expect(hashedPassword).not.toBe(password);
+		expect(hashedPassword).toBeDefined();
+		expect(isPasswordHashed).toBe(true);
+		expect(isPasswordWrong).toBe(false);
+	});
+	it('should generate a UUID', async () => {
+		const uuid = generateUUID();
+
+		expect(uuid).toBeDefined();
+		expect(typeof uuid).toBe('string');
+		expect(uuid.length).toBe(36);
+	});
+});
