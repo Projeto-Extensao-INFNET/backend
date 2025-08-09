a7ba449d31cbc1d023f5fdda21ae0c33e8a6ad48
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-08-08 23:22:32 -0300

    feat(mocks): add mocks for PrismaService and JwtService

--- test/mocks/prismaService.mock.ts
+++ test/mocks/prismaService.mock.ts
@@ -4,6 +4,12 @@ import { PrismaService } from '../../src/modules/prisma/prisma.service';
 const $connect = vi.fn();
 const $disconnect = vi.fn();
 
+const mockFindUnique = vi.fn();
+const mockFindMany = vi.fn();
+const mockCreate = vi.fn();
+const mockUpdate = vi.fn();
+const mockDelete = vi.fn();
+
 export const PrismaServiceMock = {
 	provide: PrismaService,
 	useValue: {
@@ -11,5 +17,39 @@ export const PrismaServiceMock = {
 		$disconnect,
 		onModuleInit: vi.fn().mockImplementation(() => $connect()),
 		onModuleDestroy: vi.fn().mockImplementation(() => $disconnect()),
+
+		user: {
+			findUnique: mockFindUnique,
+			findMany: mockFindMany,
+			create: mockCreate,
+			update: mockUpdate,
+			delete: mockDelete,
+		},
+
+		professional: {
+			findUnique: mockFindUnique,
+			findMany: mockFindMany,
+			create: mockCreate,
+			update: mockUpdate,
+			delete: mockDelete,
+		},
+
+		specialty: {
+			findUnique: mockFindUnique,
+			findMany: mockFindMany,
+			create: mockCreate,
+			update: mockUpdate,
+			delete: mockDelete,
+		},
+
+		typesOfTreatment: {
+			findUnique: mockFindUnique,
+			findMany: mockFindMany,
+			create: mockCreate,
+			update: mockUpdate,
+			delete: mockDelete,
+		},
 	},
 };
+
+export { mockFindUnique, mockFindMany, mockCreate, mockUpdate, mockDelete };
