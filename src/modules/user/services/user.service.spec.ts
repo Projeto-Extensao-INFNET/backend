0e658501270813a4a913bd7690ba4a2a281650a7
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-08-10 11:51:09 -0300

    test(user): add unit tests for delete user profile service

--- src/modules/user/services/user.service.spec.ts
+++ src/modules/user/services/user.service.spec.ts
@@ -1,5 +1,11 @@
+import { faker } from '@faker-js/faker/locale/pt_BR';
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
-import { PrismaServiceMock } from '../../../../test/mocks/prismaService.mock';
+import {
+	mockDelete,
+	mockFindUnique,
+	PrismaServiceMock,
+} from '../../../../test/mocks/prismaService.mock';
 import { UserService } from './user.service';
 
 describe('UserService', () => {
@@ -17,5 +23,42 @@ describe('UserService', () => {
 
 	it('should be defined', () => {
 		expect(service).toBeDefined();
+	});
+
+	it('should get user profile', async () => {});
+	it('should delete user profile', async () => {
+		const userId = 'user-123';
+
+		mockFindUnique.mockResolvedValue({
+			id: userId,
+			name: faker.person.fullName(),
+			email: faker.internet.email(),
+		});
+
+		mockDelete.mockResolvedValue({ id: userId });
+
+		await service.deleteAccount(userId);
+
+		expect(mockFindUnique).toHaveBeenCalledWith({
+			where: {
+				id: userId,
+			},
+		});
+		expect(mockDelete).toHaveBeenCalledWith({
+			where: {
+				id: userId,
+			},
+		});
+	});
+
+	it('it should throw NotFoundException when user not found', async () => {
+		const userId = 'non-existent-id';
+
+		mockFindUnique.mockResolvedValue(null);
+
+		await expect(service.deleteAccount(userId)).rejects.toThrow(
+			new NotFoundException('User not found'),
+		);
+		expect(mockDelete).not.toHaveBeenCalled();
 	});
 });
