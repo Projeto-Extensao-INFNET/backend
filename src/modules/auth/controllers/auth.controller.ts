d1809bb997cf6a7d1d7d91348dcb92881eb305e8
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-08-10 11:42:58 -0300

    fix(auth): add HTTP status code for signUp and signIn endpoints

--- src/modules/auth/controllers/auth.controller.ts
+++ src/modules/auth/controllers/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 import type { SignInDto } from '../dto/signIn.dto';
 import type { SignUpDto } from '../dto/signUp.dto';
 import { AuthService } from '../services/auth.service';
@@ -8,11 +8,13 @@ export class AuthController {
 	constructor(private readonly authService: AuthService) {}
 
 	@Post('signup')
+	@HttpCode(201)
 	signUp(@Body() body: SignUpDto) {
 		return this.authService.SignUp(body);
 	}
 
 	@Post('signin')
+	@HttpCode(201)
 	signIn(@Body() signInDto: SignInDto) {
 		return this.authService.SignIn(signInDto.email, signInDto.password);
 	}
