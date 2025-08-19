944784a2494f5aaa1bc02600efaeea5d7b71f198
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-08-18 22:15:55 -0300

    refactor(user): replace mock user repository with in-memory repository for tests

--- src/modules/user/services/user.service.spec.ts
+++ src/modules/user/services/user.service.spec.ts
@@ -1,26 +1,40 @@
 import { faker } from '@faker-js/faker/locale/pt_BR';
 import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import type { DocumentType, ROLE } from '@/_types';
+import { InMemoryUserRepository } from '../repositories/in-memory/in-memory-user.repository';
 import { UserRepository } from '../repositories/user.repository';
 import { UserService } from './user.service';
 
-const createMockUserRepository = () => ({
-	getProfile: vi.fn(),
-	findById: vi.fn(),
-	deleteAccount: vi.fn(),
-});
-
 describe('UserService', () => {
 	let service: UserService;
-	const mockUserRepository = createMockUserRepository();
+	let inMemoryUserRepository: InMemoryUserRepository; // instancio o repositório em memória de usuários
+
+	const nonExistentUserId = 'non-existent-id'; // id para testes que devem retornar erros
+	const userId = 'user-id-123'; // id para testes de sucesso
+
+	const mockUser = {
+		id: userId,
+		name: faker.person.fullName(),
+		email: faker.internet.email(),
+		password: '123455678',
+		birthDate: new Date(),
+		role: 'PATIENT' as ROLE,
+		documentType: 'CPF' as DocumentType,
+		document: faker.helpers.replaceSymbols('###.###.###-##'),
+		createdAt: new Date(),
+		updatedAt: new Date(),
+	}; // mock de um usuário
 
 	beforeEach(async () => {
+		inMemoryUserRepository = new InMemoryUserRepository(); // injeto o repositório em memória de usuários
+
 		const module: TestingModule = await Test.createTestingModule({
 			providers: [
 				UserService,
 				{
 					provide: UserRepository,
-					useValue: mockUserRepository,
+					useValue: inMemoryUserRepository, // usa a instância como provider
 				},
 			],
 		}).compile();
@@ -29,7 +43,7 @@ describe('UserService', () => {
 	});
 
 	afterEach(() => {
-		vi.resetAllMocks();
+		inMemoryUserRepository.items = []; // Limpa o array após cada teste
 	});
 
 	it('should be defined', () => {
@@ -38,66 +52,34 @@ describe('UserService', () => {
 
 	describe('getProfile', () => {
 		it('should get user profile', async () => {
-			const userId = 'user-id-123';
-			const mockUser = {
-				id: userId,
-				name: faker.person.fullName(),
-				email: faker.internet.email(),
-				password: '123455678',
-				birthDate: new Date(),
-				role: 'PATIENT',
-				documentType: 'CPF',
-				document: faker.helpers.replaceSymbols('###.###.###-##'),
-			};
-
-			mockUserRepository.getProfile.mockResolvedValue(mockUser);
+			inMemoryUserRepository.items.push(mockUser);
 
 			const result = await service.getProfile(userId);
 
-			expect(mockUserRepository.getProfile).toHaveBeenCalledWith(userId);
 			expect(result).not.toHaveProperty('password');
+			expect(result.id).toBe(userId);
 		});
 		it('it should throw NotFoundException when user not found', async () => {
-			const userId = 'non-existent-id';
-
-			mockUserRepository.getProfile.mockResolvedValue(null);
-
-			await expect(service.getProfile(userId)).rejects.toThrow(
+			await expect(service.getProfile(nonExistentUserId)).rejects.toThrow(
 				new NotFoundException('User not found'),
 			);
-			expect(mockUserRepository.getProfile).toHaveBeenCalledWith(userId);
 		});
 	});
 	describe('deleteAccount ', () => {
 		it('should delete user profile', async () => {
-			const userId = 'user-123';
-
-			mockUserRepository.findById.mockResolvedValue({
-				id: userId,
-				name: faker.person.fullName(),
-				email: faker.internet.email(),
-			});
-
-			mockUserRepository.deleteAccount.mockResolvedValue({ id: userId });
+			inMemoryUserRepository.items.push(mockUser);
 
 			await service.deleteAccount(userId);
 
-			expect(mockUserRepository.findById).toHaveBeenCalledWith(userId);
-			expect(mockUserRepository.deleteAccount).toHaveBeenCalledWith(
-				userId,
-			);
+			expect(
+				inMemoryUserRepository.items.find((user) => user.id === userId),
+			).toBeUndefined();
 		});
 
 		it('it should throw NotFoundException when user not found', async () => {
-			const userId = 'non-existent-id';
-
-			mockUserRepository.findById.mockResolvedValue(null);
-
-			await expect(service.deleteAccount(userId)).rejects.toThrow(
-				new NotFoundException('User not found'),
-			);
-			expect(mockUserRepository.deleteAccount).not.toHaveBeenCalled();
-			expect(mockUserRepository.findById).toHaveBeenCalled();
+			await expect(
+				service.deleteAccount(nonExistentUserId),
+			).rejects.toThrow(new NotFoundException('User not found'));
 		});
 	});
 	describe('createAppointment', () => {});
