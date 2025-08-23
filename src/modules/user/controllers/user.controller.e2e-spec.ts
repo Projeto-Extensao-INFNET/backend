39436d2541893e15a52efd30cdb723c6c62bb5c1
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-08-23 17:30:08 -0300

    test(user): refactor E2E tests to use factory functions for user creation and authentication

--- src/modules/user/controllers/user.controller.e2e-spec.ts
+++ src/modules/user/controllers/user.controller.e2e-spec.ts
@@ -1,11 +1,13 @@
-import { faker } from '@faker-js/faker/locale/pt_BR';
 import type { INestApplication } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 import request from 'supertest';
 import { AppModule } from '@/app.module';
+import { PrismaService } from '@/modules/prisma/prisma.service';
+import { makeAuthenticate, makeUser } from '@/test/factories';
 
 describe('User (E2E)', () => {
 	let app: INestApplication;
+	let prisma: PrismaService;
 
 	beforeAll(async () => {
 		const moduleRef = await Test.createTestingModule({
@@ -13,34 +15,14 @@ describe('User (E2E)', () => {
 		}).compile();
 
 		app = moduleRef.createNestApplication();
+		prisma = moduleRef.get<PrismaService>(PrismaService);
 
 		await app.init();
 	});
 
 	it('[DELETE] /accounts/me', async () => {
-		const fakeEmail = faker.internet.email();
-		const fakeDocument = faker.helpers.replaceSymbols('###.###.###-##');
-
-		const SignUpResponse = await request(app.getHttpServer())
-			.post('/auth/signup')
-			.send({
-				name: 'Kiko',
-				email: fakeEmail,
-				password: '12345678',
-				role: 'PATIENT',
-				birthDate: new Date(),
-				documentType: 'CPF',
-				document: fakeDocument,
-			});
-
-		const signInResponse = await request(app.getHttpServer())
-			.post('/auth/signin')
-			.send({
-				email: fakeEmail,
-				password: '12345678',
-			});
-
-		const token = signInResponse.body.accessToken;
+		const user = await makeUser(prisma);
+		const token = await makeAuthenticate(app, user.email);
 
 		const userExists = await request(app.getHttpServer())
 			.get('/accounts/me')
@@ -54,45 +36,18 @@ describe('User (E2E)', () => {
 			.get('/accounts/me')
 			.set('Authorization', `Bearer ${token}`);
 
-		expect(SignUpResponse.statusCode).toBe(201);
-		expect(signInResponse.statusCode).toBe(201);
-		expect(signInResponse.body).toHaveProperty('accessToken');
 		expect(userExists.statusCode).toBe(200);
 		expect(deleteUser.statusCode).toBe(204);
 		expect(isUserDeleted.statusCode).toBe(404);
 	});
 	it('[GET] /accounts/me', async () => {
-		const fakeEmail = faker.internet.email();
-		const fakeDocument = faker.helpers.replaceSymbols('###.###.###-##');
-
-		const SignUpResponse = await request(app.getHttpServer())
-			.post('/auth/signup')
-			.send({
-				name: 'Chaves',
-				email: fakeEmail,
-				password: '12345678',
-				role: 'PATIENT',
-				birthDate: new Date(),
-				documentType: 'CPF',
-				document: fakeDocument,
-			});
-
-		const SignInResponse = await request(app.getHttpServer())
-			.post('/auth/signin')
-			.send({
-				email: fakeEmail,
-				password: '12345678',
-			});
-
-		const token = SignInResponse.body.accessToken;
+		const user = await makeUser(prisma);
+		const token = await makeAuthenticate(app, user.email);
 
 		const getUser = await request(app.getHttpServer())
 			.get('/accounts/me')
 			.set('Authorization', `Bearer ${token}`);
 
-		expect(SignUpResponse.statusCode).toBe(201);
-		expect(SignInResponse.statusCode).toBe(201);
-		expect(SignInResponse.body).toHaveProperty('accessToken');
 		expect(getUser.statusCode).toBe(200);
 		expect(getUser.body).toHaveProperty('name');
 	});
