2ec94126ad5959e94a318cf3e66a77365a2c448a
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-08-23 17:33:04 -0300

    test(auth): refactor E2E tests to use utility functions for user data generation

--- src/modules/auth/controllers/auth.controller.e2e-spec.ts
+++ src/modules/auth/controllers/auth.controller.e2e-spec.ts
@@ -1,25 +1,19 @@
-import { faker } from '@faker-js/faker/locale/pt_BR';
 import type { INestApplication } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 import request from 'supertest';
 import { AppModule } from '@/app.module';
+import { PrismaService } from '@/modules/prisma/prisma.service';
+import { makeAuthenticate, makeUser } from '@/test/factories';
+import {
+	generateBirthDate,
+	generateUniqueDocument,
+	generateUniqueEmail,
+	generateUniqueName,
+} from '@/utils/';
 
 describe('AuthController (E2E)', () => {
 	let app: INestApplication;
-
-	const generateUniqueEmail = () => faker.internet.email();
-	const generateUniqueDocument = () =>
-		faker.helpers.replaceSymbols('##.###.###-##');
-
-	const createValidUserData = () => ({
-		name: 'Usuário Teste',
-		email: generateUniqueEmail(),
-		password: '12345678',
-		role: 'PATIENT',
-		birthDate: '2005-09-26T00:00:00.000Z',
-		documentType: 'CPF',
-		document: generateUniqueDocument(),
-	});
+	let prisma: PrismaService;
 
 	beforeAll(async () => {
 		const moduleRef = await Test.createTestingModule({
@@ -27,6 +21,7 @@ describe('AuthController (E2E)', () => {
 		}).compile();
 
 		app = moduleRef.createNestApplication();
+		prisma = moduleRef.get<PrismaService>(PrismaService);
 
 		await app.init();
 	});
@@ -41,142 +36,58 @@ describe('AuthController (E2E)', () => {
 
 	describe('SignUp ', () => {
 		it('[POST] /auth/signup - should create a new user successfully', async () => {
-			const userData = createValidUserData();
-
-			const response = await request(app.getHttpServer())
-				.post('/auth/signup')
-				.send(userData);
-
-			expect(response.statusCode).toBe(201);
-			expect(response.body).toHaveProperty('id');
-			expect(response.body).toHaveProperty('email', userData.email);
-			expect(response.body).not.toHaveProperty('password');
-		});
-
-		it('[POST] /auth/signup - should return 409 when email already exists', async () => {
-			const userData = createValidUserData();
-
-			await request(app.getHttpServer())
-				.post('/auth/signup')
-				.send(userData);
-
-			const duplicateEmailData = {
-				...userData,
+			const user = {
+				name: generateUniqueName(),
+				email: generateUniqueEmail(),
+				password: '12345678',
+				role: 'PATIENT',
+				birthDate: generateBirthDate(),
+				documentType: 'CPF',
 				document: generateUniqueDocument(),
 			};
 
 			const response = await request(app.getHttpServer())
 				.post('/auth/signup')
-				.send(duplicateEmailData);
+				.send(user);
 
-			expect(response.statusCode).toBe(409);
-			expect(response.body).toHaveProperty(
-				'message',
-				'Credentials already in use',
-			);
+			expect(response.statusCode).toBe(201);
+			expect(response.body).toHaveProperty('id');
+			expect(response.body).toHaveProperty('email', user.email);
+			expect(response.body).not.toHaveProperty('password');
 		});
+		describe('SignIn', () => {
+			it('[POST] /auth/signin - should return access token with valid credentials', async () => {
+				const user = await makeUser(prisma);
+				const token = await makeAuthenticate(app, user.email);
 
-		// !! FIX corrigir DTOs para validação correta de email
-		// it('[POST] /auth/signup - should return 400 with invalid email format', async () => {
-		// 	const userData = createValidUserData();
-
-		// 	const signUpResponse = await request(app.getHttpServer())
-		// 		.post('/auth/signup')
-		// 		.send({
-		// 			...userData,
-		// 			email: 'emailFalha.com',
-		// 		});
-
-		// 	expect([400, 500]).toContain(signUpResponse.statusCode);
-		// });
-
-		it('[POST] /auth/signup - should return 400 with missing required fields', async () => {
-			const signUpResponse = await request(app.getHttpServer())
-				.post('/auth/signup')
-				.send({ name: 'Teste' });
-
-			expect(signUpResponse.statusCode).toBe(400);
-			expect(signUpResponse.body).toHaveProperty(
-				'message',
-				'Required fields not provided',
-			);
+				expect(token).toBeDefined();
+				expect(typeof token).toBe('string');
+			});
 		});
-	});
-
-	describe('SignIn', () => {
-		it('[POST] /auth/signin - should return access token with valid credentials', async () => {
-			const userData = createValidUserData();
-
-			const signUpResponse = await request(app.getHttpServer())
-				.post('/auth/signup')
-				.send(userData);
-
-			const signinResponse = await request(app.getHttpServer())
-				.post('/auth/signin')
-				.send({
-					email: userData.email,
-					password: userData.password,
-				});
-
-			expect(signUpResponse.statusCode).toBe(201);
-			expect(signinResponse.statusCode).toBe(201);
-			expect(signinResponse.body).toHaveProperty('accessToken');
-			expect(typeof signinResponse.body.accessToken).toBe('string');
-		});
-
 		it('Complete flow: signup then signin', async () => {
-			const userData = createValidUserData();
+			const user = {
+				name: generateUniqueName(),
+				email: generateUniqueEmail(),
+				password: '12345678',
+				role: 'PATIENT',
+				birthDate: generateBirthDate(),
+				documentType: 'CPF',
+				document: generateUniqueDocument(),
+			};
 
 			const signUpResponse = await request(app.getHttpServer())
 				.post('/auth/signup')
-				.send(userData);
+				.send(user);
 
-			const signinResponse = await request(app.getHttpServer())
-				.post('/auth/signin')
-				.send({
-					email: userData.email,
-					password: userData.password,
-				});
+			const token = await makeAuthenticate(
+				app,
+				user.email,
+				user.password,
+			);
 
 			expect(signUpResponse.statusCode).toBe(201);
-			expect(signinResponse.statusCode).toBe(201);
-			expect(signinResponse.body).toHaveProperty('accessToken');
-		});
-
-		it('[POST] /auth/signin - should return 401 with invalid email', async () => {
-			const signinResponse = await request(app.getHttpServer())
-				.post('/auth/signin')
-				.send({
-					email: 'deve_falhar@acme.com',
-					password: '12345678',
-				});
-
-			expect(signinResponse.statusCode).toBe(401);
-			expect(signinResponse.body).toHaveProperty(
-				'message',
-				'Invalid credentials',
-			);
-		});
-
-		it('[POST] /auth/signin - should return 401 with invalid password', async () => {
-			const userData = createValidUserData();
-
-			await request(app.getHttpServer())
-				.post('/auth/signup')
-				.send(userData);
-
-			const signinResponse = await request(app.getHttpServer())
-				.post('/auth/signin')
-				.send({
-					email: userData.email,
-					password: 'deve_falhar_123',
-				});
-
-			expect(signinResponse.statusCode).toBe(401);
-			expect(signinResponse.body).toHaveProperty(
-				'message',
-				'Invalid credentials',
-			);
+			expect(token).toBeDefined();
+			expect(typeof token).toBe('string');
 		});
 	});
 });
