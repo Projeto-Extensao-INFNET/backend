b14ba7cf1c60d8f1cd024fb35a512bce31a08583
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-08-24 15:51:45 -0300

    refactor(types): change file extension from .d.ts to .ts

--- src/_types/index.d.ts
+++
@@ -1,3 +0,0 @@
-// Tipos dos enums do schema do Prisma
-export type ROLE = 'PATIENT' | 'PROFESSIONAL' | 'ADMIN';
-export type DocumentType = 'CPF' | 'RG';
---
+++ src/_types/index.ts
@@ -0,0 +1,7 @@
+// Tipos dos enums do schema do Prisma
+export enum ROLE {
+	PATIENT = 'PATIENT',
+	PROFESSIONAL = 'PROFESSIONAL',
+	ADMIN = 'ADMIN',
+}
+export type DocumentType = 'CPF' | 'RG';
