f3484c40f7bcf9d83bdaac1f8b51bfb701929b38
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-08-24 16:09:10 -0300

    chore(auth): remove unused console.log

--- src/modules/auth/jwt.strategy.ts
+++ src/modules/auth/jwt.strategy.ts
@@ -12,7 +12,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
 			jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
 			ignoreExpiration: false,
 		});
-		console.log('JwtStrategy instantiated');
 	}
 
 	async validate(payload: any) {
