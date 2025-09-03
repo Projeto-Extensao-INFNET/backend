8027a101ca24cdc7d55844d1f3f201ae911f1004
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-09-03 20:27:33 -0300

    feat(swagger): integrate Swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { apiReference } from '@scalar/nestjs-api-reference';
 import { AppModule } from './app.module';
 import type { Env } from './config/env';
 
@@ -12,6 +14,25 @@ async function bootstrap() {
 	const corsOrigin = configService.get('CORS_ORIGIN', { infer: true });
 
 	app.enableCors({ origin: corsOrigin ?? '*' });
+
+	/**
+	 *  SWAGGER
+	 */
+	const config = new DocumentBuilder()
+		.setTitle('API Projeto de Extensão ')
+		.setDescription('API do meu Projeto de extensão')
+		.setVersion('1.0.0')
+		.build();
+
+	const document = () => SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup('api', app, document);
+
+	app.use(
+		'/reference',
+		apiReference({
+			content: document,
+		}),
+	);
 
 	await app.listen(port ?? 3333);
 }
