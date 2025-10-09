16b507e3c5e9ec82f9066e341b7a52db438d889b
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-10-09 19:16:32 -0300

    refactor(treatment-type): update import path for TreatmentTypeService

--- src/modules/treatment-type/treatment-type.module.ts
+++ src/modules/treatment-type/treatment-type.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
-import { TreatmentTypeService } from './services/treatment-type.service';
+import { TreatmentTypeService } from '@/application/services/treatment-type.service';
 
 @Module({
 	providers: [TreatmentTypeService],
+	exports: [TreatmentTypeService],
 })
 export class TreatmentTypeModule {}
