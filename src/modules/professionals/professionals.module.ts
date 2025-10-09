724dd1fd27931e2195de9bfc57f0a4383a50c857
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-10-09 19:17:04 -0300

    refactor(professionals): streamline imports and update providers in ProfessionalsModule

--- src/modules/professionals/professionals.module.ts
+++ src/modules/professionals/professionals.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
-// import { ProfessionalsController } from './controllers/professionals.controller';
-import { ProfessionalsService } from './services/professionals.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ProfessionalsService } from '@/application/services/professionals.service';
+import { PrismaUserRepository } from '../user/repositories/prisma/prisma-user-repository';
+import { UserRepository } from '../user/repositories/user.repository';
+import { UserModule } from '../user/user.module';
 
 @Module({
-	providers: [ProfessionalsService],
-	// controllers: [ProfessionalsController],
+	imports: [UserModule, JwtModule],
+	providers: [
+		PrismaUserRepository,
+		{
+			provide: UserRepository,
+			useExisting: PrismaUserRepository,
+		},
+		ProfessionalsService,
+	],
+	exports: [ProfessionalsService, PrismaUserRepository, UserRepository],
 })
 export class ProfessionalsModule {}
