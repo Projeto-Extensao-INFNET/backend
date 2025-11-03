6f76c7a27b6598d5707e9327cab80b50b88fd28e
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-11-03 17:43:46 -0300

    fix(main): update import paths

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,29 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { setupSwagger } from './config/docs';
-import type { Env } from './config/env';
+import { setupSwagger } from './core/config/docs';
+import type { Env } from './core/config/env';
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule, {});
+  const app = await NestFactory.create(AppModule, {});
 
-	const configService = app.get<ConfigService<Env, true>>(ConfigService);
+  const configService = app.get<ConfigService<Env, true>>(ConfigService);
 
-	/**
-	 * CORS
-	 */
-	const corsOrigin = configService.get('CORS_ORIGIN', { infer: true });
-	app.enableCors({ origin: corsOrigin ?? '*' });
+  /**
+   * CORS
+   */
+  const corsOrigin = configService.get('CORS_ORIGIN', { infer: true });
+  app.enableCors({ origin: corsOrigin ?? '*' });
 
-	/**
-	 * SWAGGER
-   -> documentação disponível em 'http://localhost:3333/api'
-	 */
-	setupSwagger(app);
+  /**
+   * SWAGGER
+   */
+  setupSwagger(app);
 
-	/**
-	 * HTTP SERVER
-	 */
-	const port = configService.get('PORT', { infer: true });
-	await app.listen(port ?? 3333);
+  /**
+   * HTTP SERVER
+   */
+  const port = configService.get('PORT', { infer: true });
+  await app.listen(port ?? 3333);
 }
 bootstrap();
