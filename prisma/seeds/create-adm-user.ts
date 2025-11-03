3a60957528749ec6230ac55513b1ba08686684c9
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-11-03 17:41:09 -0300

    style(seeds): format seed files

--- prisma/seeds/create-adm-user.ts
+++ prisma/seeds/create-adm-user.ts
@@ -1,22 +1,22 @@
-import { faker } from '@faker-js/faker/locale/pt_BR';
-import { PrismaClient } from '../../generated/prisma';
-import { hashPassword } from '../../src/shared/utils';
+import { faker } from "@faker-js/faker/locale/pt_BR";
+import { PrismaClient } from "../../generated/prisma";
+import { hashPassword } from "../../src/core/shared/utils";
 
 export const CreateAdminUser = async () => {
-	const prisma = new PrismaClient();
+  const prisma = new PrismaClient();
 
-	const userAdmin = await prisma.user.create({
-		data: {
-			name: faker.person.fullName(),
-			email: faker.internet.email(),
-			password: await hashPassword('12345678'),
-			birthDate: faker.date.birthdate(),
-			role: 'ADMIN',
-			documentType: 'RG',
-			document: faker.helpers.replaceSymbols('##.###.###-#'),
-		},
-	});
-	await prisma.$disconnect();
+  const userAdmin = await prisma.user.create({
+    data: {
+      name: faker.person.fullName(),
+      email: faker.internet.email(),
+      password: await hashPassword("12345678"),
+      birthDate: faker.date.birthdate(),
+      role: "ADMIN",
+      documentType: "RG",
+      document: faker.helpers.replaceSymbols("##.###.###-#"),
+    },
+  });
+  await prisma.$disconnect();
 
-	return userAdmin;
+  return userAdmin;
 };
