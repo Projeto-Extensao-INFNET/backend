d6c56ee5b087038e4ae1cc7835f405ac428857a1
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-11-03 18:16:38 -0300

    fix(modules): remove unused module files

--- src/modules/specialty.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SpecialtyService } from '@/application/services/specialty.service';
-
-@Module({
-	providers: [SpecialtyService],
-	exports: [SpecialtyService],
-})
-export class SpecialtyModule {}
--- src/modules/treatment-type.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TreatmentTypeService } from '@services/treatment-type.service';
-
-@Module({
-	providers: [TreatmentTypeService],
-	exports: [TreatmentTypeService],
-})
-export class TreatmentTypeModule {}
