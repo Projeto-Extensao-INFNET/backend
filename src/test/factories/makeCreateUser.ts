a60ba9a9ca3b080e91ea28d51edf3ee8eca6886e
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-11-03 18:23:27 -0300

    style: format file

--- src/test/factories/makeCreateUser.ts
+++ src/test/factories/makeCreateUser.ts
@@ -1,40 +1,40 @@
-import { PrismaService } from '@/database/prisma/prisma.service';
-import { hashPassword } from '@/shared/utils';
+import { hashPassword } from '@/core/shared/utils';
 import {
-	generateBirthDate,
-	generateUniqueDocument,
-	generateUniqueEmail,
-	generateUniqueName,
-} from '@/shared/utils/generate-data';
+  generateBirthDate,
+  generateUniqueDocument,
+  generateUniqueEmail,
+  generateUniqueName,
+} from '@/core/shared/utils/generate-data';
+import { PrismaService } from '@/infra/database/prisma.service';
 
 // cria um usuário
 export const makeUser = async (prisma: PrismaService) => {
-	const result = await prisma.user.create({
-		data: {
-			name: generateUniqueName(),
-			email: generateUniqueEmail(),
-			password: await hashPassword('12345678'),
-			birthDate: generateBirthDate(),
-			role: 'PATIENT',
-			documentType: 'CPF',
-			document: generateUniqueDocument(),
-		},
-	});
+  const result = await prisma.user.create({
+    data: {
+      name: generateUniqueName(),
+      email: generateUniqueEmail(),
+      password: await hashPassword('12345678'),
+      birthDate: generateBirthDate(),
+      role: 'PATIENT',
+      documentType: 'CPF',
+      document: generateUniqueDocument(),
+    },
+  });
 
-	return result;
+  return result;
 };
 export const makeUserProfessional = async (prisma: PrismaService) => {
-	const result = await prisma.user.create({
-		data: {
-			name: generateUniqueName(),
-			email: generateUniqueEmail(),
-			password: await hashPassword('12345678'),
-			birthDate: generateBirthDate(),
-			role: 'PROFESSIONAL',
-			documentType: 'CPF',
-			document: generateUniqueDocument(),
-		},
-	});
+  const result = await prisma.user.create({
+    data: {
+      name: generateUniqueName(),
+      email: generateUniqueEmail(),
+      password: await hashPassword('12345678'),
+      birthDate: generateBirthDate(),
+      role: 'PROFESSIONAL',
+      documentType: 'CPF',
+      document: generateUniqueDocument(),
+    },
+  });
 
-	return result;
+  return result;
 };
