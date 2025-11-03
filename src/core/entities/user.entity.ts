801db615c867ca1267e32206c115d2b2d6227daa
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-11-03 18:24:23 -0300

    style(entities): update import paths on User entity

--- src/core/entities/user.entity.ts
+++ src/core/entities/user.entity.ts
@@ -1,14 +1,14 @@
-import type { DocumentType, ROLE } from '@/_types';
+import type { DocumentType, ROLE } from '@/core/shared/types';
 
 export interface UserEntity {
-	id: string;
-	name: string;
-	email: string;
-	password: string;
-	birthDate: Date;
-	role: ROLE;
-	documentType: DocumentType;
-	document: string;
-	createdAt: Date;
-	updatedAt: Date;
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+  birthDate: Date;
+  role: ROLE;
+  documentType: DocumentType;
+  document: string;
+  createdAt: Date;
+  updatedAt: Date;
 }
