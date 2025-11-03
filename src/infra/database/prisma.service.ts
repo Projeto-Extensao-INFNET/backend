c48763ead11b135de8b3abfc70c7eab7890739e9
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-11-03 18:15:38 -0300

    refactor(db): add database module to infra folder

---
+++ src/infra/database/prisma.service.ts
@@ -0,0 +1,30 @@
+import {
+  Inject,
+  Injectable,
+  type OnModuleDestroy,
+  type OnModuleInit,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PrismaClient } from 'generated/prisma';
+import { Env } from '@/core/config/env';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor(@Inject(ConfigService) configService: ConfigService<Env, true>) {
+    super({
+      log:
+        configService.get('NODE_ENV', { infer: true }) === 'development'
+          ? ['query', 'error', 'warn']
+          : ['error'],
+    });
+  }
+  onModuleInit() {
+    return this.$connect();
+  }
+  onModuleDestroy() {
+    return this.$disconnect();
+  }
+}
