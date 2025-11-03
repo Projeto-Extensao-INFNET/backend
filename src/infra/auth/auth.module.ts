64e68a3edf741a3a8837afe6db5e8ddbf9479620
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-11-03 18:03:41 -0300

    refactor(auth): create auth folder to unify files

---
+++ src/infra/auth/auth.module.ts
@@ -0,0 +1,27 @@
+import { Module, Global } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from '@/infra/auth/jwt.strategy';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+
+@Global()
+@Module({
+  imports: [
+    PassportModule,
+    ConfigModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        secret: config.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '1h' },
+      }),
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, JwtStrategy],
+  exports: [AuthService, JwtModule],
+})
+export class AuthModule {}
