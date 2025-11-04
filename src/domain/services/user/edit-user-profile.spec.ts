62714c3a5c9b2dc499f925af6748f7f07d6f5719
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-11-04 13:52:48 -0300

    refactor(services): split user services tests in multiple files

---
+++ src/domain/services/user/edit-user-profile.spec.ts
@@ -0,0 +1,57 @@
+import { CreateMockUser } from '@/test/mocks/create-mock-user/create-mock-user';
+import { Test, type TestingModule } from '@nestjs/testing';
+import { EditUserProfileService } from './edit-user-profile.service';
+import { UserRepository } from '@/infra/repositories/user.repository';
+import { PrismaUserRepository } from '@/infra/repositories/prisma/prisma-user-repository';
+import { PrismaService } from '@/infra/database/prisma.service';
+import { MockPrismaService } from '@/test/mocks/prisma';
+
+describe('editProfile', () => {
+  let service: EditUserProfileService;
+  const mockPrismaService = MockPrismaService();
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        EditUserProfileService,
+        {
+          provide: UserRepository,
+          useClass: PrismaUserRepository,
+        },
+        {
+          provide: PrismaService,
+          useValue: mockPrismaService,
+        },
+      ],
+    }).compile();
+
+    service = module.get<EditUserProfileService>(EditUserProfileService);
+  });
+
+  it('should edit user profile', async () => {
+    const user = CreateMockUser;
+
+    const dto = { name: 'Novo nome', email: 'novo@email.com' }; // dados que serão usados na edição do perfil
+
+    // primeiro valida se o usuario existe
+    mockPrismaService.user.findUnique.mockResolvedValue(user);
+
+    // simula o retorno do update
+    mockPrismaService.user.update.mockResolvedValue({
+      ...user,
+      ...dto,
+    });
+
+    // executa o método de editProfile no service
+    const result = await service.editProfile(user.id, dto);
+
+    // verifica se o update foi chamado com os dados corretos (vindos do DTO)
+    expect(mockPrismaService.user.update).toHaveBeenCalledWith({
+      where: { id: user.id },
+      data: dto,
+    });
+    expect(result).not.toHaveProperty('password'); // verifica se a senha do usuário não aparece no retorno
+    expect(result.name).toBe(dto.name); // verifica se o novo campo editado aparece corretamente
+    expect(result.email).toBe(dto.email); // verifica se o novo campo editado aparece corretamente
+  });
+});
---
+++ src/domain/services/user/get-user-profile.service.spec.ts
@@ -0,0 +1,64 @@
+import { NotFoundException } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import { nonExistentUserId } from '@/core/shared/utils';
+import { GetUserProfileService } from '@/domain/services/user/get-user-profile.service';
+import { PrismaService } from '@/infra/database/prisma.service';
+import { PrismaUserRepository } from '@/infra/repositories/prisma/prisma-user-repository';
+import { UserRepository } from '@/infra/repositories/user.repository';
+import { MockPrismaService } from '@/test/mocks/prisma';
+import { CreateMockUser } from '@/test/mocks/create-mock-user/create-mock-user';
+
+describe('GetUserProfileService', () => {
+  let service: GetUserProfileService;
+  const mockPrismaService = MockPrismaService();
+
+  const userMock = CreateMockUser;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        GetUserProfileService,
+        {
+          provide: UserRepository,
+          useClass: PrismaUserRepository,
+        },
+        {
+          provide: PrismaService,
+          useValue: mockPrismaService,
+        },
+      ],
+    }).compile();
+
+    service = module.get<GetUserProfileService>(GetUserProfileService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  describe('getUserProfileService', () => {
+    it('should get user profile', async () => {
+      const user = userMock;
+
+      mockPrismaService.user.findUnique.mockResolvedValue(userMock);
+
+      const result = await service.getUserProfile(user.id);
+
+      expect(result).not.toHaveProperty('password');
+      expect(result.id).toBe(user.id);
+    });
+
+    it('it should throw NotFoundException when user not found', async () => {
+      mockPrismaService.user.findUnique.mockResolvedValue(null);
+
+      await expect(service.getUserProfile(nonExistentUserId)).rejects.toThrow(
+        new NotFoundException('User not found'),
+      );
+    });
+  });
+
+  // describe('createAppointment', () => {});
+  // describe('getAppointments', () => {});
+  // describe('changeAppointment', () => {});
+  // describe('cancelAppointment', () => {});
+});
