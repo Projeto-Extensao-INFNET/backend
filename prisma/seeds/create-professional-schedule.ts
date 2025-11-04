9b923e61d9736f15138a442781fa1570e96511ab
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-11-04 15:13:30 -0300

    feat(seed): update fields to seed schedules table

--- prisma/seeds/create-professional-schedule.ts
+++ prisma/seeds/create-professional-schedule.ts
@@ -2,20 +2,25 @@ import { faker } from '@faker-js/faker/locale/pt_BR';
 import { PrismaClient } from '../../generated/prisma';
 
 export const CreateProfessionalSchedule = async () => {
-	const prisma = new PrismaClient();
+  const prisma = new PrismaClient();
 
-	const professionals = await prisma.professional.findMany();
+  const professionals = await prisma.professional.findMany();
 
-	for (let i = 0; i < 10; i++) {
-		const professional = faker.helpers.arrayElement(professionals);
-		await prisma.schedule.create({
-			data: {
-				schedules: faker.date.future(),
-				isAvailable: true,
-				isConfirmed: false,
-				professionalId: professional.id,
-			},
-		});
-		await prisma.$disconnect();
-	}
+  for (let i = 0; i < 20; i++) {
+    const professional = faker.helpers.arrayElement(professionals);
+
+    await prisma.schedule.create({
+      data: {
+        createdAt: faker.date.past(),
+        endAt: faker.date.past(),
+        startAt: faker.date.future(),
+        location: faker.location.city(),
+        isAvailable: true,
+        isConfirmed: false,
+        professionalId: professional.id,
+      },
+    });
+  }
+
+  await prisma.$disconnect();
 };
