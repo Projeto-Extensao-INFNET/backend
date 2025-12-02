1ffbf80fc221ca7a545f2d5ba8bec3185eec9c50
Nathan Rodrigues <nathanrodriguesvieira99@gmail.com>
2025-12-02 15:38:19 -0300

    feat(types): define new ts enuns for Prisma Schema enuns

--- src/core/shared/types/index.ts
+++ src/core/shared/types/index.ts
@@ -4,4 +4,32 @@ export enum ROLE {
   PROFESSIONAL = 'PROFESSIONAL',
   ADMIN = 'ADMIN',
 }
-export type DocumentType = 'CPF' | 'RG';
+export enum DOCUMENT_TYPE {
+  CPF = 'CPF',
+  RG = 'RG',
+  CRM = 'CRM',
+  CRP = 'CRP',
+}
+
+export enum TYPE_OF_QUERY {
+  ONLINE_VIDEO_CALL = 'ONLINE_VIDEO_CALL',
+  IN_PERSON = 'IN_PERSON',
+}
+
+export enum PAYMENT_METHOD {
+  PIX = 'PIX',
+  CREDIT_CARD = 'CREDIT_CARD',
+}
+
+export enum GENDER {
+  MALE = 'MALE',
+  FEMALE = 'FEMALE',
+  TRANSGENDER = 'TRANSGENDER',
+  NON_BINARY = 'NON_BINARY',
+}
+
+export enum QUERY_STATUS {
+  CANCELED = 'CANCELED',
+  COMPLETED = 'COMPLETED',
+  SCHEDULED = 'SCHEDULED',
+}
