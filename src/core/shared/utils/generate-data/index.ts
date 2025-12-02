7cf476d1e6005b0e7358ed3ba24be5ef68aa47ca
Nathan Rodrigues <nathanrodriguesvieira99@gmail.com>
2025-12-02 15:40:40 -0300

    feat(utils): add new document generation functions

--- src/core/shared/utils/generate-data/index.ts
+++ src/core/shared/utils/generate-data/index.ts
@@ -3,7 +3,16 @@ import { faker } from '@faker-js/faker/locale/pt_BR';
 export const nonExistentUserId = 'non-existent-id'; // id para testes que devem retornar erros
 
 export const generateUniqueName = () => faker.person.fullName();
+
 export const generateBirthDate = () => faker.date.birthdate();
+
 export const generateUniqueEmail = () => faker.internet.email();
-export const generateUniqueDocument = () =>
-  faker.helpers.replaceSymbols('##.###.###-##');
+
+export const generateUniqueCPF = () =>
+  faker.helpers.replaceSymbols('###.###.###-##');
+
+export const generateUniqueRG = () =>
+  faker.helpers.replaceSymbols('##.###.###-#');
+
+export const generateUniqueCRM = () =>
+  faker.helpers.replaceSymbols(`CRM/RJ ${faker.string.numeric(6)}`);
