c03423e9754b8e04b7a4e21050f364a8e2bacffb
Nathan Rodrigues <nathanrodriguesvieira99@gmail.com>
2025-12-06 11:18:23 -0300

    refactor(auth): remove ConfigModule and use env for JWT_SECRET

--- src/infra/auth/auth.module.ts
+++ src/infra/auth/auth.module.ts
@@ -1,21 +1,18 @@
 import { Module, Global } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from '@/infra/auth/jwt.strategy';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { env } from '@/core/config/env';
 
 @Global()
 @Module({
   imports: [
     PassportModule,
-    ConfigModule,
     JwtModule.registerAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: async (config: ConfigService) => ({
-        secret: config.get<string>('JWT_SECRET'),
+      useFactory: async () => ({
+        secret: env.JWT_SECRET,
         signOptions: { expiresIn: '1h' },
       }),
     }),
