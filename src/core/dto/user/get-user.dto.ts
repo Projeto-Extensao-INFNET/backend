203811747761431752c988cc5e7ef84cbc1bbf92
Nathan Rodrigues <nathanrodriguesvieira99@gmail.com>
2025-12-10 19:36:53 -0300

    refactor(user): separate user profile DTOs for password inclusion

--- src/core/dto/user/get-user.dto.ts
+++ src/core/dto/user/get-user.dto.ts
@@ -1,16 +1,21 @@
 import z from 'zod';
 
+// DTO para retornar perfil do usuário (SEM senha)
 export const getUserProfileDto = z.object({
   id: z.uuid(),
   name: z.string(),
   email: z.email(),
-  password: z.string(),
   birthDate: z.date(),
   role: z.enum(['PATIENT', 'PROFESSIONAL', 'ADMIN']),
   documentType: z.enum(['CPF', 'RG']),
   document: z.string(),
   createdAt: z.date(),
   updatedAt: z.date(),
+});
+
+//  DTO para retornar usuário (COM senha)
+export const getUserWithPasswordDto = getUserProfileDto.extend({
+  password: z.string(),
 });
 
 export const authenticatedUserRequest = z.object({
@@ -21,4 +26,5 @@ export const authenticatedUserRequest = z.object({
 });
 
 export type GetUserProfileDto = z.infer<typeof getUserProfileDto>;
+export type GetUserWithPasswordDto = z.infer<typeof getUserWithPasswordDto>;
 export type AuthenticatedUserRequest = z.infer<typeof authenticatedUserRequest>;
