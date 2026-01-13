8f0c54b2ed99bb8c6fe747fbf028aafa6c331593
Nathan Rodrigues <nathanrodriguesvieira99@gmail.com>
2026-01-13 14:10:22 -0300

    refactor(app): remove ConfigModule

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -1,18 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from '@/infra/database/prisma.module';
 import { AuthModule } from '../infra/auth/auth.module';
 import { HttpModule } from '../infra/http/http.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      envFilePath: '.env',
-      isGlobal: true,
-    }),
-    PrismaModule,
-    AuthModule,
-    HttpModule,
-  ],
+  imports: [PrismaModule, AuthModule, HttpModule],
 })
 export class AppModule {}
