d90756ead079183ecc5b02cf3f27b38e1dae5fd7
Nathan Rodrigues <nathanrodriguesvieira99@gmail.com>
2026-01-13 14:08:16 -0300

    feat(errors): add common error messages

---
+++ src/shared/errors/index.ts
@@ -0,0 +1,7 @@
+export const ERROR_INVALID_CREDENTIALS = 'Invalid credentials';
+export const ERROR_CREDENTIALS_IN_USE = 'Credentials already in use';
+export const ERROR_REQUIRED_FIELDS = 'Required fields not provided';
+export const ERROR_USER_NOT_FOUND = 'User not found';
+export const ERROR_SCHEDULE_NOT_FOUND = 'Schedule not found';
+export const ERROR_SCHEDULE_NOT_AVAILABLE = 'Schedule not available';
+export const ERROR_SCHEDULE_ALREADY_BOOKED = 'Schedule already booked';
