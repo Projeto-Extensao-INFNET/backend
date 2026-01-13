8e700cc9aca2792cb045e9cf1ff599e013dca96e
Nathan Rodrigues <nathanrodriguesvieira99@gmail.com>
2026-01-12 23:12:59 -0300

    fix(import): correct import paths

--- src/domain/services/user/edit-user-profile.service.ts
+++ src/domain/services/user/edit-user-profile.service.ts
@@ -1,5 +1,5 @@
-import { EditProfileDto } from '@/core/dto/user/edit-profile.dto';
-import { IUserRepository } from '@/core/repositories/user.repository';
+import { EditProfileDto } from '@/shared/dto/user/edit-profile.dto';
+import { IUserRepository } from '@/core/repositories/prisma-user-repository';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
