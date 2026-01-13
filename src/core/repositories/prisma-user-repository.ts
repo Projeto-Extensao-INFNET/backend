bbe459bf208f2ea03458d1d023559030c479e1d2
Nathan Rodrigues <nathanrodriguesvieira99@gmail.com>
2026-01-12 23:04:30 -0300

    refactor(user-repository): update prisma methods to omit user password

--- src/core/repositories/prisma-user-repository.ts
+++ src/core/repositories/prisma-user-repository.ts
@@ -2,18 +2,42 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import type { Professional } from '../../infra/database/generated/client';
 import { PrismaService } from '@/infra/database/prisma.service';
 import { UserEntity } from '@/core/entities/user.entity';
-import { EditProfileDto } from '@/core/dto/user/edit-profile.dto';
-import { IUserRepository } from '../../core/repositories/user.repository';
+import { EditProfileDto } from '@/shared/dto/user/edit-profile.dto';
+import type { GetUserProfileDto } from '@/shared/dto/user/get-user.dto';
+
+// Cria um contrato que poderá ser usado por vários repositórios reais
+export abstract class IUserRepository {
+  abstract getProfile(userId: string): Promise<GetUserProfileDto | null>;
+  abstract findById(id: string): Promise<Omit<UserEntity, 'password'>>;
+  abstract deleteProfile(id: string): Promise<void>;
+  abstract editProfile(
+    id: string,
+    dto: EditProfileDto,
+  ): Promise<EditProfileDto>;
+  abstract listProfessionals(): Promise<Professional[]>; // vai pro repo de professionals
+}
 
 // implementação real do IUserRepository usando o Prisma para acessar o banco de dados
 @Injectable()
 export class PrismaUserRepository implements IUserRepository {
   constructor(private readonly prismaService: PrismaService) {}
 
-  async findById(id: string): Promise<UserEntity | null> {
+  async findById(id: string): Promise<Omit<UserEntity, 'password'>> {
     const user = await this.prismaService.user.findUnique({
       where: {
         id,
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        password: false,
+        birthDate: true,
+        document: true,
+        documentType: true,
+        role: true,
+        createdAt: true,
+        updatedAt: true,
       },
     });
 
@@ -46,10 +70,21 @@ export class PrismaUserRepository implements IUserRepository {
     });
   }
 
-  async editProfile(id: string, dto: EditProfileDto): Promise<UserEntity> {
+  async editProfile(id: string, dto: EditProfileDto): Promise<EditProfileDto> {
     const user = await this.prismaService.user.update({
       where: { id },
       data: dto,
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        birthDate: true,
+        document: true,
+        documentType: true,
+        role: true,
+        createdAt: true,
+        updatedAt: true,
+      },
     });
 
     return user;
