f48ad60ca8249bc21250d166d58d6eb51e6f510a
Nathan Rodrigues <nathanrodriguesvieira99@gmail.com>
2026-02-15 16:36:49 -0300

    feat(factory): add makeCreateAdminUser function for creating admin users

---
+++ src/test/factories/makeCreateAdminUser.ts
@@ -0,0 +1,26 @@
+import type { PrismaService } from '@/infra/database/prisma.service';
+import type { DOCUMENT_TYPE, ROLE } from '@/shared/types';
+import {
+  generateBirthDate,
+  generateUniqueCPF,
+  generateUniqueEmail,
+  generateUniqueName,
+  hashPassword,
+} from '@/utils';
+
+// cria um usuário ADM
+export const makeCreateAdminUser = async (prisma: PrismaService) => {
+  const result = await prisma.user.create({
+    data: {
+      name: generateUniqueName(),
+      email: generateUniqueEmail(),
+      password: await hashPassword('12345678'),
+      birthDate: generateBirthDate(),
+      role: 'ADMIN' as ROLE,
+      documentType: 'CPF' as DOCUMENT_TYPE,
+      document: generateUniqueCPF(),
+    },
+  });
+
+  return result;
+};
