95a2539480f0fb1e8e339dfaf8818eb41f178f1c
Nathan Rodrigues <nathanrodriguesvieira99@gmail.com>
2026-02-15 16:43:24 -0300

    feat(appointments): add fields to createAppointmentDto for enhanced appointment details

--- src/shared/dto/appointments/create-appointment.dto.ts
+++ src/shared/dto/appointments/create-appointment.dto.ts
@@ -3,6 +3,10 @@ import { z } from 'zod';
 export const createAppointmentDto = z.object({
   scheduleId: z.string(),
   userId: z.string(),
+  specialtyId: z.string(),
+  typeOfTreatmentId: z.string(),
+  isAvailable: z.boolean(),
+  isConfirmed: z.boolean(),
 });
 
 export type CreateAppointmentDto = z.infer<typeof createAppointmentDto>;
