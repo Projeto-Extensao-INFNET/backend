4c73439bcf9b6e1175b4bf7a3c61a5f8101bbbda
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-03-04 16:45:00 -0300

    feat: add return type to execute method in GetUserProfileService

--- src/domain/services/user/get-user-profile.service.ts
+++ src/domain/services/user/get-user-profile.service.ts
@@ -1,13 +1,13 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import type { GetUserProfileResponse } from '@/shared/dto/user/get-user.dto';
 import { IUserRepository } from '@/core/repositories/prisma-user-repository';
 import { ERROR_USER_NOT_FOUND } from '@/shared/errors';
+import type { GetUserProfileResponse } from '@/shared/dto/user/get-user.dto';
 
 @Injectable()
 export class GetUserProfileService {
   constructor(private readonly repo: IUserRepository) {}
 
-  async execute(userId: string) {
+  async execute(userId: string): Promise<GetUserProfileResponse> {
     const user = await this.repo.getProfile(userId);
     if (!user) {
       throw new NotFoundException(ERROR_USER_NOT_FOUND);
