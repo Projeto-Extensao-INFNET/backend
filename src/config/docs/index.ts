dbcb572f1fea09517d099a5d983ac94b0457b2e2
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-03-04 10:39:13 -0300

    chore: update Swagger documentation title and description for clarity

--- src/config/docs/index.ts
+++ src/config/docs/index.ts
@@ -3,9 +3,13 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 export const setupSwagger = (app: INestApplication) => {
   const config = new DocumentBuilder()
-    .setTitle('API Projeto de Extensão ')
-    .setDescription('API do meu Projeto de extensão')
+    .setTitle('API Projeto de Extensão')
+    .setDescription('API do Projeto de Extensão - documentação Swagger')
     .setVersion('1.0.0')
+    .addBearerAuth(
+      { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' },
+      'authorization',
+    )
     .build();
 
   const document = () => SwaggerModule.createDocument(app, config);
