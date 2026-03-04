1a609cabfce694694c9bfaca98535354883107e4
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-03-04 10:43:27 -0300

    docs: add Swagger to HealthCheck endpoint

--- src/infra/http/controllers/health/health.controller.ts
+++ src/infra/http/controllers/health/health.controller.ts
@@ -1,8 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller()
+@ApiTags('Health')
 export class HealthController {
   @Get('/health')
+  @ApiOperation({ summary: 'Health check', operationId: 'healthCheck' })
+  @ApiResponse({ status: 200, description: 'OK' })
   async getHealth() {
     return {
       status: 'ok',
