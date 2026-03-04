c575aa9cb8a586ace0d455befc1deedb1c78b51f
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-03-04 16:47:55 -0300

    docs: update GetAppointmentsController swagger docs

--- src/infra/http/controllers/appointments/get-appointments.controller.ts
+++ src/infra/http/controllers/appointments/get-appointments.controller.ts
@@ -6,6 +6,26 @@ import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 export class GetAppointmentsController {
   @Get()
   @ApiOperation({ summary: 'Get appointments', operationId: 'getAppointment' })
-  @ApiResponse({ status: 200, description: 'OK' })
+  @ApiResponse({
+    status: 200,
+    description: 'List of appointments',
+    schema: {
+      type: 'array',
+      items: {
+        type: 'object',
+        properties: {
+          id: { type: 'string', format: 'uuid' },
+          scheduleId: { type: 'string' },
+          userId: { type: 'string' },
+          specialtyId: { type: 'string' },
+          typeOfTreatmentId: { type: 'string' },
+          isAvailable: { type: 'boolean' },
+          isConfirmed: { type: 'boolean' },
+          createdAt: { type: 'string', format: 'date-time' },
+          updatedAt: { type: 'string', format: 'date-time' },
+        },
+      },
+    },
+  })
   async getAppointments() {}
 }
