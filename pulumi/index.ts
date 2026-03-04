6854f6486ae35cd008ad67e89a7422da189bf7dc
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-03-04 10:45:59 -0300

    docs: comment out Pulumi and AWS imports in index.ts

--- pulumi/index.ts
+++ pulumi/index.ts
@@ -1,3 +1,3 @@
-import * as pulumi from '@pulumi/pulumi';
-import * as aws from '@pulumi/aws';
-import * as awsx from '@pulumi/awsx';
+// import * as pulumi from '@pulumi/pulumi';
+// import * as aws from '@pulumi/aws';
+// import * as awsx from '@pulumi/awsx';
