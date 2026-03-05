2cc854417d72e13ee6edc51309e2498dfd14a695
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-03-05 19:26:04 -0300

    refactor: update import path for PrismaModule

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { PrismaModule } from '@/infra/database/prisma.module';
+import { PrismaModule } from '@/infra/database/prisma/prisma.module';
 import { AuthModule } from '../infra/auth/auth.module';
 import { HttpModule } from '../infra/http/http.module';
 
