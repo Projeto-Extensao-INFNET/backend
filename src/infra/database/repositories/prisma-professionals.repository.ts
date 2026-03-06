44842b3ea554c307b9d05808fa1843642b1bf2ae
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-03-05 21:57:22 -0300

    feat: add test cache for professionals-repository

--- src/infra/database/repositories/prisma-professionals.repository.ts
+++ src/infra/database/repositories/prisma-professionals.repository.ts
@@ -1,9 +1,12 @@
-import { PaginationResultDto } from '@/shared/dto/pagination/pagination.dto';
 import { PrismaService } from '../prisma/prisma.service';
 import { DEFAULT_PAGE_LIMIT, DEFAULT_PAGE_NUMBER } from '@/shared/constants';
-import type { PaginationQueryDto } from '@/shared/dto/pagination/pagination.dto';
+import type {
+  PaginationQueryDto,
+  PaginationResultDto,
+} from '@/shared/dto/pagination/pagination.dto';
 import { Injectable } from '@nestjs/common';
 import type { ProfessionalEntity } from '@/core/entities/professional.entity';
+import { CacheRepository } from '@/infra/cache/cache-repository';
 
 export abstract class IProfessionalsRepository {
   abstract listProfessionals(
@@ -13,12 +16,26 @@ export abstract class IProfessionalsRepository {
 
 @Injectable()
 export class PrismaProfessionalsRepository implements IProfessionalsRepository {
-  constructor(private readonly prismaService: PrismaService) {}
+  constructor(
+    private readonly prismaService: PrismaService,
+    private readonly cache: CacheRepository,
+  ) {}
 
   async listProfessionals(
     params: PaginationQueryDto,
   ): Promise<PaginationResultDto<ProfessionalEntity>> {
     const { page = DEFAULT_PAGE_NUMBER, limit = DEFAULT_PAGE_LIMIT } = params;
+
+    const CACHE_KEY = `professionals:page:${page}:limit:${limit}`;
+
+    const cacheHit = await this.cache.get(CACHE_KEY);
+
+    if (cacheHit) {
+      const cachedData = JSON.parse(cacheHit);
+
+      return cachedData;
+    }
+
     const take = Number(limit);
     const skip = Number(page - 1) * take; // page=1, take=10 resulta em skip=0 -> primeira página com 10 itens
 
@@ -59,17 +76,21 @@ export class PrismaProfessionalsRepository implements IProfessionalsRepository {
       },
     });
 
-    const total = await this.prismaService.professional.count();
-    const totalPages = Math.ceil(total / take);
+    const total_items = await this.prismaService.professional.count();
+    const total_pages = Math.ceil(total_items / take);
 
-    return {
+    const result: PaginationResultDto<ProfessionalEntity> = {
       data: professionals,
       meta: {
-        total_items: total,
-        total_pages: totalPages,
+        total_items,
+        total_pages,
         page,
         limit,
       },
     };
+
+    await this.cache.set(CACHE_KEY, JSON.stringify(result));
+
+    return result;
   }
 }
