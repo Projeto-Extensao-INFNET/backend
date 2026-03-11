e75c3512d329dae937c67b6a16a85fa47d2102a0
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-03-11 18:24:48 -0300

    feat(auth): rename AccessTokenResponse to TokenResponse and add refreshToken property

--- src/shared/dto/auth/token-response.ts
+++ src/shared/dto/auth/token-response.ts
@@ -1,8 +1,12 @@
 import { IsString } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
-export class AccessTokenResponse {
+export class TokenResponse {
   @ApiProperty({ example: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...' })
   @IsString()
   accessToken!: string;
+
+  @ApiProperty({ example: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...' })
+  @IsString()
+  refreshToken!: string;
 }
