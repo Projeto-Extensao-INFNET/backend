cdc9c3e589efd87c3c1eb1bafded445d926600b7
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-03-11 18:31:41 -0300

    feat(auth): restructure AuthModule to include RefreshToken and Logout controllers and services

--- src/infra/auth/auth.module.ts
+++ src/infra/auth/auth.module.ts
@@ -1,11 +1,14 @@
 import { Module, Global } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
-import { JwtStrategy } from '@/infra/auth/jwt.strategy';
-import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
+import { JwtStrategy } from '@/infra/auth/jwt/jwt.strategy';
+import { AuthController } from './controllers/auth.controller';
+import { AuthService } from './services/auth.service';
 import { env } from '@/config/env';
-import { JWT_EXPIRATION } from '@/shared/constants';
+import { JWT_ACCESS_TOKEN_EXPIRATION } from '@/shared/constants';
+import { RefreshTokenController } from './controllers/refresh-token.controller';
+import { RefreshTokenService } from './services/refresh-token.service';
+import { LogoutController } from './controllers/logout.controller';
 
 @Global()
 @Module({
@@ -14,12 +17,14 @@ import { JWT_EXPIRATION } from '@/shared/constants';
     JwtModule.registerAsync({
       useFactory: async () => ({
         secret: env.JWT_SECRET,
-        signOptions: { expiresIn: JWT_EXPIRATION },
+        signOptions: {
+          expiresIn: JWT_ACCESS_TOKEN_EXPIRATION,
+        },
       }),
     }),
   ],
-  controllers: [AuthController],
-  providers: [AuthService, JwtStrategy],
+  controllers: [AuthController, RefreshTokenController, LogoutController],
+  providers: [AuthService, JwtStrategy, RefreshTokenService],
   exports: [AuthService, JwtModule],
 })
 export class AuthModule {}
