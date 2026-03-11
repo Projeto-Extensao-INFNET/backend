978916bcb6baaf84580b6da7a1034572aaf7ca9c
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-03-11 18:28:57 -0300

    feat: add cookie parser middleware to AppModule

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -1,9 +1,18 @@
-import { Module } from '@nestjs/common';
+import {
+  type MiddlewareConsumer,
+  Module,
+  type NestModule,
+} from '@nestjs/common';
 import { PrismaModule } from '@/infra/database/prisma/prisma.module';
 import { AuthModule } from '../infra/auth/auth.module';
 import { HttpModule } from '../infra/http/http.module';
+import cookieParser from 'cookie-parser';
 
 @Module({
   imports: [PrismaModule, AuthModule, HttpModule],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(cookieParser()).forRoutes('*');
+  }
+}
