641d26864b8bc843f758fc102f556ce61ed9457e
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-03-12 19:23:42 -0300

    refactor(auth): update User class structure and remove AccessTokenResponse

--- src/shared/dto/auth/auth-user.ts
+++ src/shared/dto/auth/auth-user.ts
@@ -1,17 +1,31 @@
-import { IsUUID, IsString } from 'class-validator';
+import { IsUUID, IsString, IsOptional } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
+import type { ROLE } from '@/shared/types';
 
 class User {
-  @ApiProperty({ format: 'uuid' })
-  @IsUUID()
-  userId!: string;
-
   @ApiProperty({ example: 'john_doe' })
   @IsString()
   username!: string;
+
+  @ApiProperty({ format: 'uuid' })
+  @IsUUID()
+  sub!: string;
+
+  @ApiProperty({ example: 'ADMIN' })
+  @IsString()
+  role!: ROLE;
 }
 
 export class AuthenticatedUserResponse {
   @ApiProperty({ type: User })
   user!: User;
+}
+
+export class AuthResponse {
+  @ApiProperty({ example: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...' })
+  accessToken!: string;
+
+  @ApiProperty({ example: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...' })
+  @IsOptional()
+  refreshToken!: string;
 }
