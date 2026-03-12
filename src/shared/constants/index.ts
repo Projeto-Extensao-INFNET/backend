cee088bd8046320397ada3fd104b3fffc3f7cbda
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-03-12 19:16:40 -0300

    refactor(constants): replace hardcoded values with environment variables

--- src/shared/constants/index.ts
+++ src/shared/constants/index.ts
@@ -1,22 +1,26 @@
+import { env } from '../../config/env';
+
 export const SALT_ROUNDS = 8;
 
 export const MIN_PASSWORD_LENGTH = 8;
 
-export const PORT = 3333;
+export const PORT = env.PORT;
 
 export const COOKIES_MAX_AGE = 24 * 60 * 60 * 1000; // 24h
 
-export const REDIS_PORT = 6379;
-export const REDIS_DB = 0;
-export const REDIS_HOST = '127.0.0.1';
+export const REDIS_PORT = env.REDIS_PORT;
+export const REDIS_DB = env.REDIS_DB;
+export const REDIS_HOST = env.REDIS_HOST;
 export const REDIS_CACHE_EXPIRATION = 60 * 15; // 15 min
 
-export const JWT_ACCESS_TOKEN_EXPIRATION = '15m';
-export const JWT_REFRESH_TOKEN_EXPIRATION = '24h';
+export const JWT_SECRET = env.JWT_SECRET;
+export const JWT_REFRESH_SECRET = env.JWT_REFRESH_SECRET;
+export const JWT_ACCESS_TOKEN_EXPIRATION = env.JWT_ACCESS_TOKEN_EXPIRATION;
+export const JWT_REFRESH_TOKEN_EXPIRATION = env.JWT_REFRESH_TOKEN_EXPIRATION;
 
 export const DEFAULT_PAGE_NUMBER = 1;
 export const DEFAULT_PAGE_LIMIT = 10;
 
 export const MAX_FILE_SIZE = 1024 * 1024 * 2; // 2MB
 
-export const DEV_CORS_ORIGIN = 'http://localhost:';
+export const DEV_CORS_ORIGIN = env.DEV_CORS_ORIGIN;
