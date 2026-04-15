02bc95b1e73c7bec215e9a0438ce7f78a884a712
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-04-15 17:29:55 -0300

    style(constants): organize constants

--- src/shared/constants/index.ts
+++ src/shared/constants/index.ts
@@ -1,11 +1,10 @@
 import { env } from '../../config/env';
 
 export const SALT_ROUNDS = 8;
-
 export const MIN_PASSWORD_LENGTH = 8;
 
 export const PORT = env.PORT;
-
+export const DEV_CORS_ORIGIN = env.DEV_CORS_ORIGIN;
 export const COOKIES_MAX_AGE = 60 * 15; // 15 min
 
 export const REDIS_PORT = env.REDIS_PORT;
@@ -22,5 +21,3 @@ export const DEFAULT_PAGE_NUMBER = 1;
 export const DEFAULT_PAGE_LIMIT = 10;
 
 export const MAX_FILE_SIZE = 1024 * 1024 * 2; // 2MB
-
-export const DEV_CORS_ORIGIN = env.DEV_CORS_ORIGIN;
