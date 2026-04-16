745766b3e41c3754b5fc6dc221139375bf9f5e37
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-04-16 18:35:11 -0300

    refactor(tests): simplify RefreshTokenService test by removing unused mocks

--- test/unit/infra/auth/services/refresh-token.service.spec.ts
+++ test/unit/infra/auth/services/refresh-token.service.spec.ts
@@ -1,37 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RefreshTokenService } from '@/infra/auth/services/refresh-token.service';
-import { JWTMockService } from 'test/shared/mocks/jwt';
-import { PrismaService } from '@/infra/database/prisma/prisma.service';
-import { JwtService } from '@nestjs/jwt';
-
-const mockPrisma = {
-  user: {
-    // findUnique: vi.fn(),
-    // create: vi.fn(),
-  },
-};
-
-vi.mock('@prisma/client', () => ({
-  PrismaClient: vi.fn().mockImplementation(() => mockPrisma),
-}));
 
 describe.skip('RefreshTokenService', () => {
   let service: RefreshTokenService;
-  const mockJwtService = JWTMockService();
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [
-        RefreshTokenService,
-        {
-          provide: PrismaService,
-          useValue: mockPrisma,
-        },
-        {
-          provide: JwtService,
-          useValue: mockJwtService,
-        },
-      ],
+      providers: [RefreshTokenService],
     }).compile();
 
     service = module.get<RefreshTokenService>(RefreshTokenService);
