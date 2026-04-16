676fcc2e4264b7a8c0b2bdcbe0167b70bcafec81
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-04-16 18:34:38 -0300

    refactor(tests): simplify PrismaService mock implementation in unit tests

--- test/unit/infra/database/prisma/prisma.service.spec.ts
+++ test/unit/infra/database/prisma/prisma.service.spec.ts
@@ -1,59 +1,40 @@
-import { ConfigModule } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
 import { PrismaService } from '@/infra/database/prisma/prisma.service';
 
-const mockPrisma = {
-  $connect: vi.fn(),
-  $disconnect: vi.fn(),
-  onModuleInit: vi.fn(),
-  onModuleDestroy: vi.fn(),
-};
-
-vi.mock('@prisma/client', () => ({
-  PrismaClient: vi.fn().mockImplementation(() => mockPrisma),
-}));
-
 describe('PrismaService', () => {
-  let service: PrismaService;
+  let prisma: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [ConfigModule.forRoot({ isGlobal: true })],
       providers: [
         {
           provide: PrismaService,
-          useValue: mockPrisma,
+          useValue: {
+            $connect: vi.fn(),
+            $disconnect: vi.fn(),
+            onModuleInit: vi.fn(),
+            onModuleDestroy: vi.fn(),
+          },
         },
       ],
     }).compile();
 
-    service = module.get<PrismaService>(PrismaService);
+    prisma = module.get<PrismaService>(PrismaService);
   });
 
-  afterEach(() => {
-    vi.resetAllMocks();
-  });
-
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('service should be defined', () => {
+    expect(prisma).toBeDefined();
   });
 
   it('should call $connect', async () => {
-    mockPrisma.onModuleInit.mockImplementation(() => {
-      mockPrisma.$connect();
-    });
-
-    await service.onModuleInit();
-
-    expect(service.$connect).toHaveBeenCalledTimes(1);
+    vi.spyOn(prisma, '$connect').mockResolvedValue();
+    await prisma.$connect();
+    expect(prisma.$connect).toHaveBeenCalledTimes(1);
   });
+
   it('should call $disconnect', async () => {
-    mockPrisma.onModuleDestroy.mockImplementation(() => {
-      mockPrisma.$disconnect();
-    });
-
-    await service.onModuleDestroy();
-
-    expect(service.$disconnect).toHaveBeenCalledTimes(1);
+    vi.spyOn(prisma, '$disconnect').mockResolvedValue();
+    await prisma.$disconnect();
+    expect(prisma.$disconnect).toHaveBeenCalledTimes(1);
   });
 });
