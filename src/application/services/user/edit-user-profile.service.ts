4c843f5c9e463ff35c097985b2528008ea87e814
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-04-21 17:06:52 -0300

    refactor(service): update EditUserProfileService to improve return type and error handling

--- src/application/services/user/edit-user-profile.service.ts
+++ src/application/services/user/edit-user-profile.service.ts
@@ -1,16 +1,21 @@
 import { EditProfileDto } from '@/infra/http/dtos/user/edit-profile.dto';
 import { IUserRepository } from '@/infra/database/repositories/prisma-user-repository';
 import { Injectable } from '@nestjs/common';
+import { ok, type Result } from '@/shared/errors/result';
 
 @Injectable()
 export class EditUserProfileService {
   constructor(private readonly repo: IUserRepository) {}
 
-  async execute(id: string, dto: EditProfileDto): Promise<EditProfileDto> {
+  async execute(
+    id: string,
+    dto: EditProfileDto,
+  ): Promise<Result<EditProfileDto>> {
     await this.repo.findById(id);
 
     const updatedUser = await this.repo.editProfile(id, dto);
+    if (!updatedUser.ok) return updatedUser;
 
-    return updatedUser;
+    return ok(updatedUser.value);
   }
 }
