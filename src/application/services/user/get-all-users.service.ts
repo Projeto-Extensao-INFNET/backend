cd31cf9b066c0519894a9605c49566055f67fb06
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-04-21 17:08:58 -0300

    refactor(service): enhance error handling and return type in GetAllUsersService

--- src/application/services/user/get-all-users.service.ts
+++ src/application/services/user/get-all-users.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { IUserRepository } from '@/infra/database/repositories/prisma-user-repository';
+import { ok, type Result } from '@/shared/errors/result';
+
 import type { UserModel } from '@/domain/models/user.model';
 import type {
   PaginationQueryDto,
@@ -12,7 +14,9 @@ export class GetAllUsersService {
 
   async execute(
     query: PaginationQueryDto,
-  ): Promise<PaginationResultDto<Omit<UserModel, 'password'>>> {
-    return this.repo.getAllUsers(query);
+  ): Promise<Result<PaginationResultDto<Omit<UserModel, 'password'>>>> {
+    const users = await this.repo.getAllUsers(query);
+    if (!users.ok) return users;
+    return ok(users.value);
   }
 }
