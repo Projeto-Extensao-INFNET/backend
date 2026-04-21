a28954fd8d638eaebc8cd163ed1cad400686159e
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-04-21 17:32:56 -0300

    refactor(repo): update createAppointment method to improve error handling and return type

--- src/infra/database/repositories/prisma-appointments.repository.ts
+++ src/infra/database/repositories/prisma-appointments.repository.ts
@@ -1,15 +1,16 @@
-import type { PrismaService } from '../prisma/prisma.service';
-import type { CreateAppointmentDto } from '@/infra/http/dtos/appointments/create-appointment.dto';
+import { ok, type Result } from '@/shared/errors/result';
+import { CreateAppointmentDto } from '@/infra/http/dtos/appointments/create-appointment.dto';
 import {
-  ERROR_SCHEDULE_ALREADY_BOOKED,
-  ERROR_SCHEDULE_NOT_AVAILABLE,
-  ERROR_SCHEDULE_NOT_FOUND,
-  ERROR_USER_NOT_FOUND,
-} from '@/shared/errors';
-import { ConflictException, NotFoundException } from '@nestjs/common';
+  conflict,
+  resourceNotFound,
+} from '@/shared/errors/exceptions/exceptions';
+
+import type { PrismaService } from '../prisma/prisma.service';
 
 export abstract class IPrismaAppointmentsRepository {
-  abstract createAppointment: (data: CreateAppointmentDto) => Promise<void>;
+  abstract createAppointment: (
+    data: CreateAppointmentDto,
+  ) => Promise<Result<CreateAppointmentDto>>;
 }
 
 export class PrismaAppointmentsRepository
@@ -17,16 +18,16 @@ export class PrismaAppointmentsRepository
 {
   constructor(private readonly prismaService: PrismaService) {}
 
-  async createAppointment(data: CreateAppointmentDto): Promise<void> {
-    await this.prismaService.$transaction(async (prisma) => {
+  async createAppointment(
+    data: CreateAppointmentDto,
+  ): Promise<Result<CreateAppointmentDto>> {
+    return this.prismaService.$transaction(async (prisma) => {
       // Valida se existe um usuário
       const userExists = await prisma.user.findUnique({
         where: { id: data.userId },
       });
 
-      if (!userExists) {
-        throw new NotFoundException(ERROR_USER_NOT_FOUND);
-      }
+      if (!userExists) resourceNotFound('Usuário não encontrado!');
 
       // Valida se Agenda do Profissional existe e possíveis conflitos/falta de campos
       const scheduleExists = await prisma.schedule.findUnique({
@@ -34,27 +35,20 @@ export class PrismaAppointmentsRepository
         include: { UserAgenda: true, professional: true },
       });
 
-      if (!scheduleExists) {
-        throw new NotFoundException(ERROR_SCHEDULE_NOT_FOUND);
-      }
+      if (!scheduleExists) resourceNotFound('Agendamento não encontrado!');
 
-      if (!scheduleExists.isAvailable) {
-        throw new ConflictException(ERROR_SCHEDULE_NOT_AVAILABLE);
-      }
+      if (!scheduleExists?.isAvailable) conflict('Agendamento indisponível!');
 
-      if (scheduleExists.UserAgenda) {
-        throw new ConflictException(ERROR_SCHEDULE_ALREADY_BOOKED);
-      }
+      if (scheduleExists?.UserAgenda) conflict('Agendamento indisponível!');
 
-      if (scheduleExists.professional.specialtyId !== data.specialtyId) {
-        throw new ConflictException('');
-      }
+      if (scheduleExists?.professional.specialtyId !== data.specialtyId)
+        conflict('Agendamento indisponível!');
 
       if (
-        scheduleExists.professional.typeOfTreatmentId !== data.typeOfTreatmentId
-      ) {
-        throw new ConflictException('');
-      }
+        scheduleExists?.professional.typeOfTreatmentId !==
+        data.typeOfTreatmentId
+      )
+        conflict('Agendamento indisponível!');
 
       // Cria o agendamento
       await prisma.userAgenda.upsert({
@@ -89,6 +83,8 @@ export class PrismaAppointmentsRepository
           status: 'COMPLETED',
         },
       });
+
+      return ok(data);
     });
   }
 }
