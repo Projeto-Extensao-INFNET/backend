07b62f8d1c92117abb932d90d566df44e9430a0e
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-04-21 17:03:03 -0300

    refactor(service): improve error handling and return type in AvatarUploadService

--- src/application/services/upload-avatar/avatar-upload.service.ts
+++ src/application/services/upload-avatar/avatar-upload.service.ts
@@ -1,7 +1,14 @@
+import { Injectable, Request } from '@nestjs/common';
+
 import { IUserRepository } from '@/infra/database/repositories/prisma-user-repository';
+
+import {
+  badRequest,
+  resourceNotFound,
+} from '@/shared/errors/exceptions/exceptions';
+import { err, ok, type Result } from '@/shared/errors/result';
+
 import type { AuthenticatedUserResponse } from '@/infra/http/dtos/auth/auth-user';
-import { ERROR_USER_NOT_FOUND } from '@/shared/errors';
-import { Injectable, NotFoundException, Request } from '@nestjs/common';
 
 @Injectable()
 export class AvatarUploadService {
@@ -9,21 +16,19 @@ export class AvatarUploadService {
   async exec(
     @Request() req: AuthenticatedUserResponse,
     avatarFile: Express.Multer.File,
-  ): Promise<void> {
+  ): Promise<Result<string>> {
     const userId = req.user.sub;
 
-    if (!avatarFile) {
-      // TODO - lançar erro
-    }
+    if (!avatarFile) return err(badRequest('Arquivo do avatar é obrigatório'));
 
     const userExists = await this.repo.findById(userId);
-    if (!userExists) {
-      throw new NotFoundException(ERROR_USER_NOT_FOUND);
-    }
+    if (!userExists) return err(resourceNotFound('Usuário não encontrado'));
 
     const convertAvatarToBase64 = avatarFile.buffer.toString('base64');
     const avatarUrlData = `data:${avatarFile.mimetype};base64,${convertAvatarToBase64}`;
 
     await this.repo.uploadAvatar(userId, avatarUrlData);
+
+    return ok('Imagem de perfil enviada com sucesso!');
   }
 }
