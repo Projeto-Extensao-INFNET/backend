bcfc74f3dc62025fce4e69ac44f399d88c419bac
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-04-27 09:21:48 -0300

    fix(imports): update import paths for Auth module to reflect new directory structure

--- src/infra/auth/auth.module.ts
+++ src/infra/auth/auth.module.ts
@@ -2,13 +2,13 @@ import { Module, Global } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { AccessTokenStrategy } from '@/infra/auth/jwt/access-token.strategy';
-import { AuthController } from './controllers/auth.controller';
-import { AuthService } from './services/auth.service';
+import { AuthController } from '../../presentation/controllers/auth/auth.controller';
+import { AuthService } from '../../application/services/auth/auth.service';
 import { JWT_ACCESS_TOKEN_EXPIRATION, JWT_SECRET } from '@/shared/constants';
-import { RefreshTokenController } from './controllers/refresh-token.controller';
-import { LogoutController } from './controllers/logout.controller';
+import { RefreshTokenController } from '../../presentation/controllers/auth/refresh-token.controller';
+import { LogoutController } from '../../presentation/controllers/auth/logout.controller';
 import { RefreshTokenStrategy } from './jwt/refresh-token.strategy';
-import { RefreshTokenService } from './services/refresh-token.service';
+import { RefreshTokenService } from '../../application/services/auth/refresh-token.service';
 import { GetTokens } from './jwt/generate-jwt-tokens';
 
 @Global()
