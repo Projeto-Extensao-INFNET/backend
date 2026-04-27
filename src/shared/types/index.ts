f00bb237b602a5848438f6b1b1c9da7bb74ad14d
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-04-27 09:18:59 -0300

    feat(types): add OmittedUserPassword type

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -1,3 +1,5 @@
+import type { UserModel } from '@/domain/models/user.model';
+
 export type ROLE = 'PATIENT' | 'PROFESSIONAL' | 'ADMIN';
 export type DOCUMENT_TYPE = 'CPF' | 'RG';
 export type PROFESSIONAL_DOCUMENT_TYPE = 'CRM' | 'CRP';
@@ -5,8 +7,11 @@ export type TYPE_OF_QUERY = 'ONLINE_VIDEO_CALL' | 'IN_PERSON';
 export type PAYMENT_METHOD = 'PIX' | 'CREDIT_CARD';
 export type GENDER = 'MALE' | 'FEMALE' | 'TRANSGENDER' | 'NON_BINARY';
 export type QUERY_STATUS = 'CANCELED' | 'COMPLETED' | 'SCHEDULED';
+
 export type Payload = {
   username: string;
   sub: string;
   role: ROLE;
 };
+
+export type OmittedUserPassword = Omit<UserModel, 'password'>;
